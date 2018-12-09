3a03569dad4c7ebbb5798bf26075a9f75a3deb11
eric.zhang <eric@ericzhangmatoMacBook-Pro.local>
2018-12-09 18:16:24 +0800

    init
    init

---
+++ SunFrontend/src/pages/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Analysis extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+
+      }
+  }
+
+  render() {
+    return (
+      <div>home</div>
+    )
+  }
+}
+
+export default Analysis;
